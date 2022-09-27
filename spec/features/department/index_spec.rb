1e3d017e0f2510f54700ba91a22eae6b8043fa7b
Alex M <alexm0717@hotmail.com>
2022-09-27 09:12:02 -0700

    end of story one

--- spec/features/department/index_spec.rb
+++ spec/features/department/index_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe("Department Index page") do
   before(:each) do
     @department1 = Department.create!(    name: "dept1",     floor: "first")
     @department2 = Department.create!(    name: "dept2",     floor: "second")
+    @employee1 = Employee.create!(    name: "alex",     level: 1,     department_id: @department1.id)
+    @employee2 = Employee.create!(    name: "Gabe",     level: 2,     department_id: @department1.id)
+    @employee3 = Employee.create!(    name: "Peyton",     level: 3,     department_id: @department2.id)
   end
 
   it("I see each department's name and floor") do
@@ -16,5 +19,11 @@ RSpec.describe("Department Index page") do
   end
 
   it("  And underneath each department, I can see the names of all of its employees") do
+    visit(departments_path)
+    save_and_open_page
+    expect(page).to(have_content("Dept name:#{@department1.name}"))
+    expect(page).to(have_content("Dept name:#{@department2.name}"))
+    expect(page).to(have_content("Employee name:#{@employee1.name}"))
+    expect(page).to(have_content("Employee name:#{@employee3.name}"))
   end
 end
